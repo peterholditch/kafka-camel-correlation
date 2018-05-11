f47fe165d5f19a61945f8fc62c419997de5f31b3
David Hope <30470909+appdynamicsdh@users.noreply.github.com>
2018-05-11 13:37:59 -0400

    Changes to support non PTP messaging

--- src/main/java/com/appdynamics/KafkaProducerInstrumentation.java
+++ src/main/java/com/appdynamics/KafkaProducerInstrumentation.java
@@ -10,10 +10,9 @@ import com.appdynamics.instrumentation.sdk.template.AExit;
 import com.appdynamics.instrumentation.sdk.toolbox.reflection.IReflector;
 import com.appdynamics.instrumentation.sdk.toolbox.reflection.ReflectorException;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public class KafkaProducerInstrumentation extends AExit {
 
@@ -22,6 +21,9 @@ public class KafkaProducerInstrumentation extends AExit {
     private IReflector getHeaders = null;
     private IReflector addHeader = null;
     private IReflector topic = null;
+
+    Collection<UUID> li = new ArrayList<UUID>(10);
+    final BlockingQueue<UUID> queue = new ArrayBlockingQueue<>(li.size(), false, li);
 
     private boolean haveCorrelation = false;
 
@@ -40,6 +42,10 @@ public class KafkaProducerInstrumentation extends AExit {
         topic = getNewReflectionBuilder().invokeInstanceMethod("topic", searchSuperClass)
                 .build();
 
+        //Build a list of 10 Unique Ids
+        for(int i=0;i<10;i++) {
+            queue.add(UUID.randomUUID());
+        }
     }
 
     @Override
@@ -87,9 +93,16 @@ public class KafkaProducerInstrumentation extends AExit {
 
     @Override
     public Map<String, String> identifyBackend(Object invokedObject, String className, String methodName, Object[] paramValues, Throwable thrownException, Object returnValue, ISDKUserContext context) throws ReflectorException {
+
         Map<String, String> map = new HashMap<String, String>();
         Object o = paramValues[0];
-        map.put("Kafka", topic.execute(o.getClass().getClassLoader(), o));
+        String topicStr = topic.execute(o.getClass().getClassLoader(), o);
+
+        //Add one of 10 uuids to the back end, round robin to avoid backend tier fighting
+        UUID uuid = queue.poll();
+        map.put("Kafka", topicStr+uuid.toString());
+        queue.add(uuid);
+
         return map;
     }
 
